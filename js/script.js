da42486362967026e293bf01570699f52c70ee92
Stephen Chang <stephenycchang@gmail.com>
2015-09-23 22:34:47 -0400

    Updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-$('.name').on('click', function(e){
-  $('.name').text('Stephen Chang');
+$(document).ready(function() {
+  $('.enter').click(function(e) {
+    $(".enter").animate({
+      height: 'toggle'
+    });
+  });
+
+  $(window).on('scroll', function() {
+    // window height
+    var w = $(window).height();
+    w = (w/2);
+    // number of px scrolled
+    var px = $(window).scrollTop();
+
+    if (px > w) {
+      // $('.topnav').fadeIn();
+      $('.topnav').fadeIn();
+    } else {
+      $('.topnav').fadeOut();
+    }
+  });
 });
